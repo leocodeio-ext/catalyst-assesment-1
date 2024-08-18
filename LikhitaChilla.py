55d8b2265eb9106467d549d3e93a41d5f9b928b8
LikhitaChilla <chillalikhita1242@gmail.com>
2024-08-18 22:48:27 +0530

    youtube-transcriptor

--- LikhitaChilla.py
+++ LikhitaChilla.py
@@ -1,6 +1,6 @@
 import os
 import google.generativeai as genai
-import streamlit as st
+# import streamlit as st
 from dotenv import load_dotenv
 model=genai.GenerativeModel('gemini-1.5-flash')
 chat=model.start_chat(history=[])
@@ -13,12 +13,7 @@ from dotenv import load_dotenv
 load_dotenv()
 genai.configure(api_key=os.getenv('GOOGLE_API_KEY'))
 
-model = genai.GenerativeModel('gemini-1.5-flash')
-chat = model.start_chat(history=[])
 
-imp=''''Till now you read a user query.Now this is your task:You are an expert in summarizing youtube video.You will be taking the transcript text
-and also the user query .so you need to carefully analyze the transcript text and answer the query accordingly.
-The transcript text will be appended here:'''
 def extract_transcript(youtube_video_url):
     try:
         video_id = youtube_video_url.split('=')[1]
@@ -28,13 +23,25 @@ def extract_transcript(youtube_video_url):
     except Exception as e:
         return f"Error extracting transcript: {str(e)}"
 
-def generate_content(video_url, prom,imp):
+def generate_content(video_url, prom):
     transcript_text = extract_transcript(video_url)
+
+    model = genai.GenerativeModel('gemini-1.5-flash')
+    chat = model.start_chat(history=[])
+
+    imp = f"""The line before this is the user query or his feelings he want to express to you or he is most probably trying to talk to you.Now this is your task:Either You are an expert in summarizing youtube video.You will be taking the transcript text
+    and also the user query .so you need to carefully analyze the transcript text and answer the query accordingly.
+    or you are an expert who can answer any kind of questions if you are not provided with the transcript text
+    The transcript text will be appended here if the user wants an answer from the video else if he wants from you he will just ask a query 
+    you have to answer it in anyway.
+    The user input: 
+    The transcript text is append here:"""
+
     response = chat.send_message(str(prom)+str(imp)+str(transcript_text))
     return response
 
 def chatbot_interface(video_url, prom):
-    response = generate_content(video_url, prom,imp)
+    response = generate_content(video_url, prom)
     return response.text
 # Create the Gradio interface
 interface = gr.Interface(
@@ -50,4 +57,4 @@ interface = gr.Interface(
 )
 
 # Launch the interface
-interface.launch()
+interface.launch(share=True)
